37e87903f7c9426599ae836addb2afbdc7d699d5
BettyWang <wangyuwen@wangyuwens-MacBook-Pro.local>
2018-09-15 13:54:15 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const moment = require('moment')
+const notifier = require('node-notifier')
+const path = require('path')
+const remote = require('electron').remote
+
+const elNow = document.querySelector('.now-time')
+const elAlarm = document.querySelector('.alarm-time')
+elAlarm.addEventListener('change', onAlarmTextChange)
+
+let time = moment()
+
+let nowTime
+let alarmTime
+
+/** Set Time */
+const now = moment(time).format('HH:mm:ss')
+nowTime = now
+elNow.innerText = now
+
+const alarm = moment(time).add(5, 'seconds').format('HH:mm:ss')
+alarmTime = alarm
+elAlarm.value = alarm
+
+timer()
+
+/** Now Time */
+function timer() {
+    time = moment().format('HH:mm:ss')
+
+    /** Set Now */
+    nowTime = time
+    elNow.innerText = time
+
+    check();
+
+    setTimeout(() => {
+        timer()
+    }, 1000)
+}
+
+/** Check Time */
+function check() {
+    const diff = moment(nowTime, 'HH:mm:ss').diff(moment(alarmTime, 'HH:mm:ss'))
+    if (diff === 0) {
+        const msg = "It's" + alarmTime + ". Wake Up!"
+        /** const msg = `It's ${alarmTime}. Wake Up!` */
+        notice(msg)
+    }
+}
+
+/**
+ * System Notification
+ * @param {string} msg
+ */
+function notice(msg) {
+    /** Show Form */
+    const window = remote.getCurrentWindow()
+    window.restore()
+    window.show()
+
+    /** https://github.com/mikaelbr/node-notifier */
+    notifier.notify({
+        title: 'Alarm Clock',
+        message: msg,
+        icon: path.join(__dirname, 'clock.ico'),
+        sound: true,
+    })
+}
+
+/**
+ * Save To Global Variable,
+ * Can't Read Dom In Minimize Status.
+ * @param {event} event
+ */
+function onAlarmTextChange(event) {
+    alarmTime = event.target.value
+}
---
+++ main.js
@@ -0,0 +1,81 @@
+const {
+    app,
+    BrowserWindow,
+    Tray,
+    Menu,
+} = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    // darwin = MacOS
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
+
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 400,
+        height: 400,
+        maximizable: false
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Open DevTools.
+    // win.webContents.openDevTools()
+
+    // When Window Close.
+    win.on('closed', () => {
+        win = null
+    })
+
+    // When Window Minimize
+    win.on('minimize', () => {
+        win.hide()
+    })
+
+    // Create Tray
+    createTray()
+
+}
+
+function createTray() {
+    let appIcon = null
+    const iconPath = path.join(__dirname, 'clock.ico')
+
+    const contextMenu = Menu.buildFromTemplate([{
+            label: 'AlarmClock',
+            click() {
+                win.show()
+            }
+        },
+        {
+            label: 'Quit',
+            click() {
+                win.removeAllListeners('close')
+                win.close()
+            }
+        }
+    ]);
+
+    appIcon = new Tray(iconPath)
+    appIcon.setToolTip('Alarm Clock')
+    appIcon.setContextMenu(contextMenu)
+
+}
